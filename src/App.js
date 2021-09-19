ffe06b413cad431e14040a8eb0e899fe3cd3e183
alamin500 <nalamin500@gmail.com>
2021-09-20 00:00:18 +0600

    j

--- src/App.js
+++ src/App.js
@@ -4,15 +4,27 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <Mycomponent brand="Apple" price="5000"></Mycomponent>
+      <Mycomponent brand="Microsoft" price="10000"></Mycomponent>
+      <Mycomponent brand="Google" price="0"></Mycomponent>
       <Mycomponent></Mycomponent>
     </div>
   );
 }
 
-function Mycomponent(){
+function Mycomponent(props){
+  console.log(props)
+  const myStyle ={
+    backgroundColor: 'lightblue',
+    border: '3px solid blue',
+    margin: '20px',
+    padding: '10px'
+  }
   return(
-    <div>
-      <h1>Yo yo mama. My own component</h1>
+    <div style={myStyle}>
+      <h1>Yo yo mama.{props.brand}</h1>
+      <h4>Asking money {props.price}</h4>
+      <p style={{ color: 'red', fontWeight: 'bold'}}>I can write my own conponent</p>
     </div>
   )
 }

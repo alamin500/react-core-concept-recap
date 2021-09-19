a7652f84fc1ca3d939a11e462699e79442e57c1e
alamin500 <nalamin500@gmail.com>
2021-09-19 23:29:32 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Mycomponent></Mycomponent>
+    </div>
+  );
+}
+
+function Mycomponent(){
+  return(
+    <div>
+      <h1>Yo yo mama. My own component</h1>
+    </div>
+  )
+}
+export default App;

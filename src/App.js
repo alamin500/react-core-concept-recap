f150985065f4155c70969030d63e6f469abcd53e
alamin500 <nalamin500@gmail.com>
2021-09-20 07:25:39 +0600

    f

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
@@ -13,17 +14,23 @@ function App() {
 }
 
 function Mycomponent(props){
-  console.log(props)
+  const [points, setPoints] = useState(1)
   const myStyle ={
     backgroundColor: 'lightblue',
     border: '3px solid blue',
     margin: '20px',
-    padding: '10px'
+    padding: '10px',
+    borderRadius: '10px'
+  }
+  const handleAddPoints = () => {
+    const newPoints = points * 2;
+    setPoints(newPoints);
   }
   return(
     <div style={myStyle}>
-      <h1>Yo yo mama.{props.brand}</h1>
+      <h1>Yo yo mama.{props.brand}, I hava points:{points}</h1>
       <h4>Asking money {props.price}</h4>
+      <button onClick={handleAddPoints}>Add Points</button>
       <p style={{ color: 'red', fontWeight: 'bold'}}>I can write my own conponent</p>
     </div>
   )
